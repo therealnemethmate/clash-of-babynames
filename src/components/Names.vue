3a2b6d585a54d50434746ba03048ab40a8df84e3
Mate Nemeth <mate.nemeth@oander.hu>
2023-09-27 22:14:25 +0200

    init: clash of babynames

---
+++ src/components/Names.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import namesData from '../assets/girl-names.json';
+
+const names = ref<string[]>(namesData.names);
+
+</script>
+
+<template>
+  <div>
+    <h1>Girl names</h1>
+    <div v-for="name in names" :key="name">{{ name }}</div>
+  </div>
+</template>
